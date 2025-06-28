11344abcb819e57482e704fed2227881b877240d
devbud2y <devbuddy99@gmail.com>
2025-06-28 21:28:55 +0900

    refactor: 포인트 충전 및 사용 기능에 ReentrantLock을 활용한 동시성 제어로 리팩토링

--- src/main/java/com/study/tdd/point/ChargePointUseCase.java
+++ src/main/java/com/study/tdd/point/ChargePointUseCase.java
@@ -5,21 +5,29 @@ import com.study.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static com.study.tdd.point.TransactionType.CHARGE;
 
 @Service
 @RequiredArgsConstructor
 public class ChargePointUseCase {
 
+    private final Lock lock = new ReentrantLock(true);
+
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
     public UserPoint charge(Long userId, Long amount, Long updateMillis) {
         UserPoint chargedUserPoint;
-        synchronized (this) {
+        lock.lock();
+        try {
             UserPoint userPoint = userPointTable.selectById(userId);
             chargedUserPoint = userPoint.charge(amount, updateMillis);
             userPointTable.insertOrUpdate(userId, chargedUserPoint.point());
+        } finally {
+            lock.unlock();
         }
         pointHistoryTable.insert(userId, amount, CHARGE, updateMillis);
         return chargedUserPoint;
--- src/main/java/com/study/tdd/point/UsePointUseCase.java
+++ src/main/java/com/study/tdd/point/UsePointUseCase.java
@@ -5,21 +5,29 @@ import com.study.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static com.study.tdd.point.TransactionType.USE;
 
 @Service
 @RequiredArgsConstructor
 public class UsePointUseCase {
 
+    private final Lock lock = new ReentrantLock(true);
+
     private final UserPointTable userPointTable;
     private final PointHistoryTable pointHistoryTable;
 
     public UserPoint use(Long userId, Long amount, Long updateMillis) {
         UserPoint usedUserPoint;
-        synchronized (this) {
+        lock.lock();
+        try {
             UserPoint userPoint = userPointTable.selectById(userId);
             usedUserPoint = userPoint.use(amount, updateMillis);
             userPointTable.insertOrUpdate(userId, usedUserPoint.point());
+        } finally {
+            lock.unlock();
         }
         pointHistoryTable.insert(userId, amount, USE, updateMillis);
         return usedUserPoint;
